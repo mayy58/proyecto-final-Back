79769044cbc53578a6ba2dded2ee912583ef03a4
Ivatroth <100944229+Ivatroth@users.noreply.github.com>
2023-05-10 18:28:52 -0300

    Update getProduct.js

--- api/src/handlers/getProduct.js
+++ api/src/handlers/getProduct.js
@@ -1,13 +1,19 @@
-const findAllProduct = require("../controllers/findAllProduct");
+const findAllProduct = require("../controllers/findAllProduct")
+const getByNameProduct = require("../controllers/getByNameProduct")
 
-const getAllProduct = async (req, res) => {
-  try {
-    const prod = await findAllProduct();
-    res.status(200).json(prod);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+const getProduct = async (req, res) => {
+    let prod={};
+    try {
+        const {name} = req.query;
+        name ?
+            prod = await getByNameProduct(name)
+        :
+            prod = await findAllProduct()
 
-console.log(prod);
-module.exports = getAllProduct;
+        res.status(200).json(prod);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+ }
+
+module.exports = getProduct;
