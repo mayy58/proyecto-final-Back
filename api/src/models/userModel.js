fb28b8177d34b7c99dc7527bafa08f37be5c4063
ivatroth <ivana.frontroth@gmail.com>
2023-05-19 21:23:23 -0300

    rutas de orden y users

--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -1,3 +1,65 @@
+// const { DataTypes } = require("sequelize");
+// const bcrypt = require("bcrypt");
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//   // defino el modelo
+//   sequelize.define(
+//     "user",
+//     {
+//       id: {
+
+//         type: DataTypes.INTEGER,
+
+//         primaryKey: true,
+//         autoIncrement: true,
+//       },
+//       name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       lastName: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       birthDate: {
+//         type: DataTypes.DATEONLY,
+//         allowNull: false,
+//       },
+//       email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       password: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         set(value) {
+//           const hashedPassword = bcrypt.hashSync(value, 10);
+//           this.setDataValue("password", hashedPassword);
+//         },
+//       },
+//       nickname: {
+//         type: DataTypes.STRING,
+//       },
+
+//       address: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       roll: {
+//         type: DataTypes.ENUM("ADMIN", "USER", "SELLER"),
+//         defaultValue: "USER",
+//       },
+//       deleteLogic: {
+//         type: DataTypes.BOOLEAN,
+//         defaultValue: true,
+//       },
+//     },
+//     {
+//       timestamps: false,
+//     }
+//   );
+// };
 const { DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 // Exportamos una funcion que define el modelo
@@ -8,9 +70,7 @@ module.exports = (sequelize) => {
     "user",
     {
       id: {
-
         type: DataTypes.INTEGER,
-
         primaryKey: true,
         autoIncrement: true,
       },
@@ -24,7 +84,6 @@ module.exports = (sequelize) => {
       },
       birthDate: {
         type: DataTypes.DATEONLY,
-        allowNull: false,
       },
       email: {
         type: DataTypes.STRING,
@@ -32,7 +91,7 @@ module.exports = (sequelize) => {
       },
       password: {
         type: DataTypes.STRING,
-        allowNull: false,
+
         set(value) {
           const hashedPassword = bcrypt.hashSync(value, 10);
           this.setDataValue("password", hashedPassword);
@@ -44,11 +103,13 @@ module.exports = (sequelize) => {
 
       address: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       roll: {
-        type: DataTypes.ENUM("ADMIN", "USER", "SELLER"),
+        type: DataTypes.ENUM("USER", "ADMIN", "SELLER"),
         defaultValue: "USER",
+      },
+      googleId: {
+        type: DataTypes.STRING,
       },
       deleteLogic: {
         type: DataTypes.BOOLEAN,
