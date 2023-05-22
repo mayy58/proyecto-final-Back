f4e9f153603ce66e116c7cc1158cca30973b3a6b
mayy58 <103051601+mayy58@users.noreply.github.com>
2023-05-21 21:29:39 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -21,6 +21,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false,
   native: false,
+});
 
 detailOrderModel(sequelize);
 productModel(sequelize);
