633068d109cf42485e2e6594fc821b7bb2cd8116
TomasSandoval <tomassandoval079@gmail.com>
2023-05-21 22:32:59 -0300

    no pull

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
