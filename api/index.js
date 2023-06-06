4fff8eca4ffdb0acad46d6f2b7362b3253c7e028
ivatroth <ivana.frontroth@gmail.com>
2023-06-06 20:43:52 -0300

    probamos con puerto 80 para el deploy

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
